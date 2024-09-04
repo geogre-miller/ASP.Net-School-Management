a51204e3451935068e676ffa898b8df6e7a3206f
trungnine123 <quanghuy00433@gmail.com>
2024-09-04 17:39:33 +0700

    updated

---
+++ Class.aspx.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using MySql.Data.MySqlClient; // Changed from System.Data.SqlClient to MySql.Data.MySqlClient
+
+public partial class _Class : System.Web.UI.Page
+{
+    string constring = "server=127.0.0.1; user=root; database=lms; password=";
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        using (MySqlConnection Sqlcon = new MySqlConnection(constring)) // Changed to MySqlConnection
+        {
+            string query = "select * from Product"; // Adjusted spacing in query
+            Sqlcon.Open();
+            MySqlDataAdapter sqlData = new MySqlDataAdapter(query, Sqlcon); // Changed to MySqlDataAdapter
+            DataTable dataTable = new DataTable();
+            sqlData.Fill(dataTable);
+            GridView1.DataSource = dataTable;
+            GridView1.DataBind();
+        }
+    }
+}
---
+++ EditAttendance.aspx.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using MySql.Data.MySqlClient;
+
+public partial class EditAttendance : System.Web.UI.Page
+{
+    string constring = "server=127.0.0.1; user=root; database=lms; password=";
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        using (MySqlConnection Sqlcon = new MySqlConnection(constring))
+        {   
+            try
+            {
+                string query = "select * from `attendance`";
+                Sqlcon.Open();
+                MySqlDataAdapter sqlData = new MySqlDataAdapter(query, Sqlcon);
+                DataTable dataTable = new DataTable();
+                sqlData.Fill(dataTable);
+                GridView1.DataSource = dataTable;
+                GridView1.DataBind();
+            } catch (Exception ex)
+            {
+                Response.Write(ex);
+            }
+        }
+    }
+
+    protected void Gstd_Click(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void EditAtt_Click(object sender, EventArgs e)
+    {
+
+    }
+}
